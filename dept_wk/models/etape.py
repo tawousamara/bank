36445ced72a495795141a3d1cf0ca431e9f46f59
tawousama <kt_amara@esi.dz>
2024-07-03 15:56:03 +0200

    version 02/07

--- dept_wk/models/etape.py
+++ dept_wk/models/etape.py
@@ -13,7 +13,7 @@ import arabic_reshaper
 from odoo.exceptions import ValidationError, UserError
 import magic
 import xlrd
-import pandas as pd
+#import pandas as pd
 
 
 List_items = ['هل العميل شخص مقرب سياسيا؟',
@@ -557,7 +557,7 @@ class Etape(models.Model):
     active = fields.Boolean(default=True)
     can_edit = fields.Boolean(string='', compute='compute_readonly')
     can_edit_finance = fields.Boolean(string='', compute='compute_readonly_finance')
-    template_situation = fields.Binary(string='Template situation comptable', compute='compute_template_xls')
+    """template_situation = fields.Binary(string='Template situation comptable', compute='compute_template_xls')
     template_name = fields.Char(default='Template situation comptable.xls')
 
     def compute_template_xls(self):
@@ -594,7 +594,7 @@ class Etape(models.Model):
 
             file_base64 = base64.b64encode(buffer.read()).decode('utf-8')
 
-            rec.template_situation = file_base64
+            rec.template_situation = file_base64"""
 
     @api.depends('risque_ids')
     def _compute_company_fisc(self):
